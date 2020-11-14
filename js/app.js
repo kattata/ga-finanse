cde56c88dbe08d534dcf9dfac8dee254f2910109
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-11-14 02:07:49 +0100

    small changes to nav

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // NAVIGATION
-let burger = document.querySelector('.fa-bars');
+let burger = document.querySelector('.burger');
 let nav = document.querySelector('.nav-items');
-
-
 
 function toggleNav() {
     nav.classList.toggle('open-nav');
+    burger.classList.toggle('open-burger');
 }
 
 burger.addEventListener('click', toggleNav);
