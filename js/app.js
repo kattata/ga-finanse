40eb7e2ccd6d69e57ea4490183fcd6431e047655
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-11-18 19:27:02 +0100

    glide js almost responsive

--- js/app.js
+++ js/app.js
@@ -8,3 +8,27 @@ function toggleNav() {
 }
 
 burger.addEventListener('click', toggleNav);
+
+// GLIDE CAROUSEL
+const config = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 5,
+    gap: 20,
+    breakpoints: {
+        1400: {
+            perView: 4
+        },
+        1150: {
+            perView: 3
+        },
+        980: {
+            perView: 4
+        },
+        790: {
+            perView: 3
+        }
+    }
+}
+
+new Glide('.trusted-container', config).mount();
